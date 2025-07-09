da3e6b606a96e70bbe5988d6eceaebf0fdf36eaa
M. Haziem Nabawi <haziemnabawi481@gmail.com>
2025-07-09 12:19:45 +0700

    mas nabil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="bg-white rounded-lg shadow-md p-8">
 
           <div className="w-24 h-24 mx-auto bg-blue-500 rounded-full flex items-center justify-center text-white text-2xl font-bold mb-6">
-            YN
+            MHN
           </div>
           
           <h1 className="text-3xl font-bold text-gray-800 mb-4">
@@ -15,7 +15,7 @@ export default function Home() {
           </h1>
           
           <p className="text-gray-600 mb-6">
-            Seorang Web Developer yang  Newbie dan  belum bisa apa apa
+            Seorang Web Developer Newbie yang belum bisa apa apa
           </p>
           
           <Link 
